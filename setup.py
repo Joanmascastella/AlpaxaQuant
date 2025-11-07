9e6ff8390ec8eb8defc8d07063086a17858a25f9
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2025-11-07 15:03:32 +0900

    refactoring

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alpaxa_quant",
-    version="0.1.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         "beautifulsoup4==4.14.2",
